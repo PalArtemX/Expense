2ddcea72d185fd753fe53e2c0880a0d6c09c942e
Artem Paliutin <pal.artem@icloud.com>
2022-04-03 13:44:39 +0300

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by Artem Paliutin on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
